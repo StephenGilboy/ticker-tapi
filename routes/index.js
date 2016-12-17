2fdbd376a650f88fc793c22172af2ab58747f783
Stephen Gilboy <sgilboy@outlook.com>
2016-12-17 11:28:58 -0800

    Cleaned up files. Added description on index page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Ticker TAPI', description: 'Socket IO app to send fake stock ticker data for learning reactive programming.'});
 });
 
 module.exports = router;
